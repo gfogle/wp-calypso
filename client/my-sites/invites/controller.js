bed1f66b58ba59bfea660b200d497ecf1efd11e8
Miguel Lezama <miguel@automattic.com>
2015-12-21 11:56:55 -0300

    Invites: use global notices list

--- client/my-sites/invites/controller.js
+++ client/my-sites/invites/controller.js
@@ -10,6 +10,7 @@ import React from 'react';
 import i18n from 'lib/mixins/i18n';
 import titleActions from 'lib/screen-title/actions';
 import InviteAccept from 'my-sites/invites/invite-accept';
+import notices from 'lib/invites/notices';
 
 export function acceptInvite( context ) {
 	titleActions.setTitle( i18n.translate( 'Accept Invite', { textOnly: true } ) );
